8a850e2a3b726af137b36f2963f4eca293d936bc
Phillip-Juan <phillip-juan@zailab.com>
2021-03-09 22:13:28 +0200

    clean up locale component

--- easer-app/src/components/language-selector/language-selector.ts
+++ easer-app/src/components/language-selector/language-selector.ts
@@ -1,12 +1,16 @@
 import { autoinject, customElement } from 'aurelia-framework';
 import { I18N } from 'aurelia-i18n';
 
+import './language-selector.scss';
+
 @autoinject()
 @customElement('language-selector')
 export class LanguageSelector {
 
   public locales: any;
   public currentLocale: any;
+  public selectedLocale: any;
+  public visible: boolean;
 
   constructor(private i18n: I18N) {
     this.locales = [{
@@ -17,15 +21,26 @@ export class LanguageSelector {
       code: "af"
     }];
     this.currentLocale = this.i18n.getLocale();
+    this.selectedLocale = this.locales.find(loc => loc.code === this.currentLocale).title;
     console.log(' ::>> this.currentLocale >>>> ', this.currentLocale);
   }
 
 
-  public setLocale(locale: { code: string }): void {
+  public setLocale(locale: { title: string, code: string }): void {
     let code = locale.code
     if(this.currentLocale !== code) {
       this.i18n.setLocale(code);
       this.currentLocale = code;
+      this.selectedLocale = locale.title;
     }
+    this.hide();
+  }
+
+  public toggle(): void {
+    this.visible = !this.visible;
+  }
+
+  public hide(): void {
+    this.visible = false;
   }
 }
