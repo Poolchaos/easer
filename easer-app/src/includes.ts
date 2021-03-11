f38f3779ef8d9e85d2ce9375dcacfb22442889a0
Phillip-Juan <phillip-juan@zailab.com>
2021-03-11 19:32:10 +0200

    implement contact page

--- easer-app/src/includes.ts
+++ easer-app/src/includes.ts
@@ -1,5 +1,6 @@
 import '@fortawesome/fontawesome-free/scss/fontawesome.scss';
 import '@fortawesome/fontawesome-free/scss/brands.scss';
+import '@fortawesome/fontawesome-free/scss/solid.scss';
 import '@fortawesome/fontawesome-free/js/solid.js';
 import '@fortawesome/fontawesome-free/js/brands.js';
 import '@fortawesome/fontawesome-free/js/fontawesome.js';
